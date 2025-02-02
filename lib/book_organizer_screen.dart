cbcb72483f09c4319039bdc495074ca81e8868bd
japka444 <yarej7285@gmail.com>
2025-02-02 10:40:33 -0800

    Update book_organizer_screen.dart

--- lib/book_organizer_screen.dart
+++ lib/book_organizer_screen.dart
@@ -2,3 +2,22 @@ class BookOrganizerScreen extends StatefulWidget {
   @override
   _BookOrganizerScreenState createState() => _BookOrganizerScreenState();
 }
+
+// For managing the bottom navigation
+  int _selectedIndex = 0;
+
+  // Handle Bottom Navigation Changes
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    // Navigate to the appropriate screen based on the selected index
+    if (index == 0) {
+      // Navigate to Home (Login screen)
+      Navigator.pushReplacementNamed(context, '/');
+    } else if (index == 1) {
+      // Navigate to User Profile screen
+      Navigator.pushReplacementNamed(context, '/profile');
+    }
+  }
